1f7036677afb92418c6dd6ea0199b8ed1d89c91d
Наталья Кузьмина <natakkuzmina@gmail.com>
2023-02-25 21:51:04 +0300

    change if test to assert

--- first_test/tests/test_capitalize.py
+++ first_test/tests/test_capitalize.py
@@ -1,11 +1,8 @@
 from first_test.capitalize import capitalize
 
 # Если результат функции не равен ожидаемому значению
-if capitalize('hello') != 'Hello':
-    # Выбрасываем исключение и завершаем выполнение теста
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+# Выбрасываем исключение и завершаем выполнение теста
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
 
 print('Все тесты пройдены!')
